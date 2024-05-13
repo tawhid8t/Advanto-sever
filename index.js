e512079818531f9006217cc2283a97c4b3f8d83d
tawhid8t <tawhidulislam861625@gmail.com>
2024-05-13 21:56:34 +0600

    send data from database to client successfully

--- index.js
+++ index.js
@@ -35,6 +35,11 @@ async function run() {
         const result = await spotsCollection.insertOne(newSpot)
         res.send(result)
     })
+    app.get('/allSpots', async(req, res)=>{
+        const cursor = spotsCollection.find()
+        const result = await cursor.toArray()
+        res.send(result)
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
